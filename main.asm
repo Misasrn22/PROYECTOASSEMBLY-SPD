bd57a09be3e1282ebc8fe875277ceff641091682
Leandro Rios <100501693+lEANDROmati@users.noreply.github.com>
2025-11-17 22:40:14 -0300

    mas funciones

--- main.asm
+++ main.asm
@@ -3,57 +3,133 @@
 .stack 100h
 
 .data
-    CADENA DB "BOCA JUNIORS",0dh,0ah,24h
+    
+    CADENA DB "2000 >>>>>>",0dh,0ah,24h
+    CADENA2 DB "BOCA JUNIORS 200",0dh,0ah,24h
+
+    char_color db 15
+    texto_anim db "HOLA$"
+
+
+    
 
 .code
+; IMPORTAMOS LA FUNCIÓN
 extrn draw_char:proc
-extrn draw_sprites:proc
+extrn draw_pattern:proc
 extrn draw_text:proc
-extrn sprite_cat:byte
-extrn sprite_alien:byte 
-extrn sprite_hearth:byte
-extrn sprite_bomb:byte
-extrn sprite_space_invader:byte
-extrn sprite_pac:byte
-extrn sprite_ghost:byte
-extrn sprite_mushroom:byte
+extrn draw_cuadricula:proc
+extrn draw_dotted_pattern:proc
+extrn draw_marco:proc
+extrn draw_marco_offset:proc
+extrn animar_texto_lr:proc
+
 
 main proc
     mov ax, @data
     mov ds, ax
 
+    ; --- Modo gráfico 320x200x256
     mov ah, 00h
     mov al, 13h
     int 10h
+;------------------------------------------------------------------------------------------
+    jmp draw_dotted_pattern_ ;saltar a la etiqueta de la funcion preferida para la visualizacion
+;_--------------------------------------------------------------------------------------------
+draw_pattern_:
+    mov al, 1      ; color para cuadros pares
+    mov bl, 0     ; color para cuadros impares
+    call draw_pattern
+    jmp finpatron
+draw_cuadricula_:
+    mov al, 15    ; blanco (fondo)
+    mov bl, 14     ; azul claro (líneas)
+    mov cl, 4     ; tamaño de cuadrícula
+    call draw_cuadricula
+    jmp finpatron
 
-    ; Dibujar texto
-    mov cx, 96
-    mov dx, 96
-    mov si, offset CADENA
+draw_dotted_pattern_:
+    mov al, 15    ; fondo negro
+    mov bl, 15   ; puntos gris claro
+    mov cl, 6    ; separación horizontal
+    mov ch, 6    ; separación vertical
+    call draw_dotted_pattern
+    jmp finpatron
+draw_marco_:
+    mov al, 14         ; rojo claro
+    call draw_marco
+    jmp finpatron
+
+draw_marco_offset_:
+    mov al, 14      ; amarillo
+    mov bx, 10      ; distancia al borde
+    mov cx, 300     ; ancho max 319
+    mov dx, 180     ; alto
+    call draw_marco_offset
+    jmp finpatron
+
+
+    ; Dibujar letra A en (100, 50)
+    ;Entrada:
+    ;AL = carácter ('A','E','I','O','U' mayúsculas)
+    ;CX = pos X
+    ;DX = pos Y
+finpatron:
+           ; color amarillo
+   ; mov byte ptr [char_color], 12   ; rojo
+    ;mov dx, 100                     ; Y = 100
+    ;call animar_texto_lr
+    
+ ;   mov cx, 320 ; cx=pos x
+  ;  mov dx, 320
+   ; mov al, '0'
+    ;call draw_char
+
+    ;_-
+    ;
+
+
+    mov al, 1        ; rojo claro
+    call draw_marco
+     
+
+    mov al, 1      ; amarillo
+    mov bx, 9      ; distancia al borde
+    mov cx, 300     ; ancho max 319
+    mov dx, 180     ; alto
+    call draw_marco_offset
+
+
+
+
+    
+    
+
+    mov cx, 70  ; X
+    mov dx, 96     ; Y
+    mov si,offset cadena  
     call draw_text
 
-    ; REINICIALIZAR DESPUÉS DE DRAW_TEXT
-    mov ax, @data
-    mov ds, ax
-
-    ; Dibujar corazón (sprite_hearth, no sprite_heart)
-    mov si, offset sprite_hearth
-    mov cx, 50
-    mov dx, 50
-    mov al, 12
-    call draw_sprites
 
 
-texto:
+   
+
+  
+
+
+    
+
+    ; Esperar tecla
     mov ah, 00h
     int 16h
-    jmp texto
-
+    
+    ; Volver a texto
     mov ah, 00h
     mov al, 03h
     int 10h
 
-    mov ax, 4C00h
+    
+    mov ax,4C00h
     int 21h
 main endp
 
