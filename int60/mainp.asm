28e11b2ab55a698c824b2a2c53c8b88f99ebbcd4
Leandro Rios <100501693+lEANDROmati@users.noreply.github.com>
2025-11-19 01:56:40 -0300

    Modify string and adjust display coordinates
    Updated string and modified coordinates for display.

--- int60/mainp.asm
+++ int60/mainp.asm
@@ -3,33 +3,35 @@
 .stack 100h
 
 .data
-CADENA db "HOLA MUNDO$"
+CADENA db "AAAAA0 A0$"    
 
 .code
-start:
+main proc
     mov ax, @data
     mov ds, ax
+    mov es, ax        ; IMPORTANTE para el TSR
 
+    ; modo gráfico 13h
     mov ax, 0013h
     int 10h
 
-    ; pasar segmento correcto al TSR
-    push cs
-    pop es
-
+    ; llamar INT 60h
     mov ah, 01h
-    mov bl, 12         ; color
-    mov cx, 50         ; X
-    mov dx, 100        ; Y
+    mov bl, 12        ; color
+    mov cx, 0        ; X
+    mov dx, 60        ; Y
     mov si, offset CADENA
     int 60h
 
+    ; esperar tecla
     mov ah, 00h
     int 16h
 
+    ; volver a modo texto
     mov ax, 0003h
     int 10h
 
     mov ax, 4C00h
     int 21h
-end start
+main endp
+end main
