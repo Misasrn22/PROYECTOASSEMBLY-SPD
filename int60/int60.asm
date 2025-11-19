de2f29c24f4985e9f827689a9eb352d46f176a95
Leandro Rios <100501693+lEANDROmati@users.noreply.github.com>
2025-11-19 16:11:50 -0300

    FUNCIONA

--- int60/int60.asm
+++ int60/int60.asm
@@ -1,197 +1,181 @@
-; int60_tsr_fixed.asm
-; TSR .COM (tiny). Servicio 01: dibujar texto 8x8.
-; Compilar: tasm int60_tsr_fixed.asm
-;          tlink /t int60_tsr_fixed.obj
-
 .8086
 .model tiny
 .code
 org 100h
 
 start:
-    jmp main        ; primer código del .COM
+    jmp instalar
 
-; --------------------------
-; BLOQUE RESIDENTE (todo esto debe quedar en memoria)
-; --------------------------
+; TABLA COMPLETA DE FONTS (A-Z, 0-9, símbolos)
+fonts:
+; A-Z
+db 00111100b, 01000010b, 01000010b, 01111110b, 01000010b, 01000010b, 01000010b, 00000000b  ; A
+db 01111100b, 01000010b, 01000010b, 01111100b, 01000010b, 01000010b, 01111100b, 00000000b  ; B  
+db 00111100b, 01000010b, 01000000b, 01000000b, 01000000b, 01000010b, 00111100b, 00000000b  ; C
+db 01111100b, 01000010b, 01000010b, 01000010b, 01000010b, 01000010b, 01111100b, 00000000b  ; D
+db 01111110b, 01000000b, 01000000b, 01111100b, 01000000b, 01000000b, 01111110b, 00000000b  ; E
+db 01111110b, 01000000b, 01000000b, 01111100b, 01000000b, 01000000b, 01000000b, 00000000b  ; F
+db 00111100b, 01000010b, 01000000b, 01001110b, 01000010b, 01000010b, 00111100b, 00000000b  ; G
+db 01000010b, 01000010b, 01000010b, 01111110b, 01000010b, 01000010b, 01000010b, 00000000b  ; H
+db 01111100b, 00010000b, 00010000b, 00010000b, 00010000b, 00010000b, 01111100b, 00000000b  ; I
+db 00000110b, 00000010b, 00000010b, 00000010b, 01000010b, 01000010b, 00111100b, 00000000b  ; J
+db 01000010b, 01000100b, 01001000b, 01110000b, 01001000b, 01000100b, 01000010b, 00000000b  ; K
+db 01000000b, 01000000b, 01000000b, 01000000b, 01000000b, 01000000b, 01111110b, 00000000b  ; L
+db 01000010b, 01100110b, 01011010b, 01000010b, 01000010b, 01000010b, 01000010b, 00000000b  ; M
+db 01000010b, 01100010b, 01010010b, 01001010b, 01000110b, 01000010b, 01000010b, 00000000b  ; N
+db 00111100b, 01000010b, 01000010b, 01000010b, 01000010b, 01000010b, 00111100b, 00000000b  ; O
+db 01111100b, 01000010b, 01000010b, 01111100b, 01000000b, 01000000b, 01000000b, 00000000b  ; P
+db 00111100b, 01000010b, 01000010b, 01000010b, 01001010b, 01000100b, 00111010b, 00000000b  ; Q
+db 01111100b, 01000010b, 01000010b, 01111100b, 01001000b, 01000100b, 01000010b, 00000000b  ; R
+db 00111100b, 01000010b, 01000000b, 00111100b, 00000010b, 01000010b, 00111100b, 00000000b  ; S
+db 01111111b, 00001000b, 00001000b, 00001000b, 00001000b, 00001000b, 00001000b, 00000000b  ; T
+db 01000010b, 01000010b, 01000010b, 01000010b, 01000010b, 01000010b, 00111100b, 00000000b  ; U
+db 01000010b, 01000010b, 01000010b, 01000010b, 00100100b, 00011000b, 00000000b, 00000000b  ; V
+db 01000010b, 01000010b, 01000010b, 01000010b, 01011010b, 01100110b, 01000010b, 00000000b  ; W
+db 01000010b, 00100100b, 00011000b, 00011000b, 00100100b, 01000010b, 01000010b, 00000000b  ; X
+db 01000010b, 00100100b, 00011000b, 00001000b, 00001000b, 00001000b, 00001000b, 00000000b  ; Y
+db 01111110b, 00000100b, 00001000b, 00010000b, 00100000b, 01000000b, 01111110b, 00000000b  ; Z
 
-old60_ofs dw 0
-old60_seg dw 0
+; 0-9
+db 00111100b, 01000110b, 01001010b, 01010010b, 01100010b, 01000010b, 00111100b, 00000000b  ; 0
+db 00011000b, 00111000b, 00011000b, 00011000b, 00011000b, 00011000b, 01111110b, 00000000b  ; 1
+db 00111100b, 01000010b, 00000010b, 00011100b, 00100000b, 01000000b, 01111110b, 00000000b  ; 2
+db 00111100b, 01000010b, 00000010b, 00011100b, 00000010b, 01000010b, 00111100b, 00000000b  ; 3
+db 00000100b, 00001100b, 00010100b, 00100100b, 01111110b, 00000100b, 00000100b, 00000000b  ; 4
+db 01111110b, 01000000b, 01111100b, 00000010b, 00000010b, 01000010b, 00111100b, 00000000b  ; 5
+db 00111100b, 01000000b, 01000000b, 01111100b, 01000010b, 01000010b, 00111100b, 00000000b  ; 6
+db 01111110b, 00000010b, 00000100b, 00001000b, 00010000b, 00010000b, 00010000b, 00000000b  ; 7
+db 00111100b, 01000010b, 01000010b, 00111100b, 01000010b, 01000010b, 00111100b, 00000000b  ; 8
+db 00111100b, 01000010b, 01000010b, 00111110b, 00000010b, 00000010b, 00111100b, 00000000b  ; 9
 
-; minimal fonts (ejemplo): A, espacio y 0 (agregar más si querés)
-font_A db 00111100b,01100110b,11000011b,11000011b,11111111b,11000011b,11000011b,00000000b
-font_0 db 00111100b,01100110b,11001111b,11011011b,11110011b,01100110b,00111100b,00000000b
-font_SPACE db 00000000b,00000000b,00000000b,00000000b,00000000b,00000000b,00000000b,00000000b
+; Símbolos (índices 36+)
+db 00000000b, 00000000b, 00000000b, 00000000b, 00000000b, 00000000b, 00000000b, 00000000b  ; espacio (36)
+db 00000000b, 00000000b, 00000000b, 00000000b, 00000000b, 00011000b, 00011000b, 00000000b  ; . (37)
+db 00011000b, 00011000b, 00011000b, 00011000b, 00000000b, 00011000b, 00011000b, 00000000b  ; ! (38)
 
-char_color db 15
-current_x  dw 0
+old_int60_offset dw 0
+old_int60_segment dw 0
+color_actual db 15
 
-; --------------------------
-; draw_font_8x8: SI -> puntero al font (8 bytes), CX=X, DX=Y
-; --------------------------
-draw_font_8x8 proc
+; TABLA DE BÚSQUEDA RÁPIDA
+char_table db 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .!'
+
+; HANDLER INT 60h - CON BÚSQUEDA EN TABLA
+int60_handler proc far
+    cmp ah, 01h
+    jne chain_old
+    
+    push es ds si di bp bx cx dx ax
+    push cs
+    pop ds
+    
+    mov [color_actual], bl
+    mov bp, cx
+    mov di, dx
+    
+bucle_caracteres:
+    mov al, es:[si]
+    cmp al, '$'
+    je fin_texto
+    
+    ; BÚSQUEDA EN TABLA - SIN SALTOS LARGOS
+    mov bx, offset char_table
+    mov cx, 39  ; Total de caracteres en la tabla
+    
+buscar_en_tabla:
+    cmp al, [bx]
+    je encontrado
+    inc bx
+    loop buscar_en_tabla
+    jmp siguiente_car  ; No encontrado
+    
+encontrado:
+    ; Calcular offset del font
+    mov ax, 39         ; Total caracteres
+    sub ax, cx         ; AX = índice encontrado
+    mov bl, 8
+    mul bl            ; AX = índice * 8
+    mov bx, offset fonts
+    add bx, ax        ; BX apunta al font
+
+dibujar_car:
+    push si
+    mov si, bx
+    mov cx, bp
+    mov dx, di
+    call dibujar_font
+    pop si
+    
+siguiente_car:
+    add bp, 9
+    inc si
+    jmp bucle_caracteres
+
+fin_texto:
+    pop ax dx cx bx bp di si ds es
+    iret
+
+chain_old:
+    pushf
+    call dword ptr cs:[old_int60_offset]
+    iret
+
+; FUNCIÓN DIBUJAR FONT
+dibujar_font proc
     push ax bx cx dx si di
-    xor bh,bh
-    mov [current_x], cx
     mov di, 8
-fila_loop:
+    mov bh, 0
+    
+bucle_filas:
     mov bl, [si]
-    mov cx, [current_x]
-    push dx
-    mov bp, 8
-col_loop:
-    test bl,10000000b
-    jz no_px
-    mov ah,0Ch
-    mov al, [char_color]
+    mov cx, bp
+    push di
+    mov di, 8
+    
+bucle_columnas:
+    test bl, 10000000b
+    jz no_pixel
+    mov ah, 0Ch
+    mov al, [color_actual]
     int 10h
-no_px:
-    shl bl,1
+no_pixel:
+    shl bl, 1
     inc cx
-    dec bp
-    jnz col_loop
-    pop dx
+    dec di
+    jnz bucle_columnas
+    pop di
     inc dx
     inc si
     dec di
-    jnz fila_loop
+    jnz bucle_filas
     pop di si dx cx bx ax
     ret
-draw_font_8x8 endp
+dibujar_font endp
 
-; --------------------------
-; HANDLER INT 60h (FAR)
-; Prologue/Epilogue balanceados y DS=CS dentro del handler
-; --------------------------
-int60_handler proc far
-    ; salvar registros (orden consistente)
-    push ax
-    push bx
-    push cx
-    push dx
-    push si
-    push di
-    push bp
-    push ds
-    push es
-
-    ; ahora establecemos DS=CS para acceder a las tablas residentes
-    push cs
-    pop ds
-
-    ; --- servicio: AH = 01 -> dibujar texto usando ES:SI como fuente de cadena ---
-    cmp ah, 01h
-    je svc_01
-
-    ; no atendido -> encadenar al handler original
-    jmp encadenar_viejo
-
-; ------------------------------------------------
-svc_01:
-    ; Bl = color, CX = X, DX = Y, ES:SI -> cadena terminada en '$'
-    mov [char_color], bl
-    mov bx, cx        ; X actual
-    mov di, dx        ; Y fijo
-
-svc01_loop:
-    mov al, es:[si]    ; LEEMOS desde ES:SI (caller debe pasar ES)
-    cmp al, '$'
-    je svc01_done
-
-    ; localizar font simple: ejemplo A/0/space (expandir a tabla si querés)
-    cmp al, 'A'
-    je use_A
-    cmp al, '0'
-    je use_0
-    cmp al, ' '
-    je use_space
-    jmp svc01_skip_char
-
-use_A:
-    mov si, offset font_A
-    jmp svc01_draw
-use_0:
-    mov si, offset font_0
-    jmp svc01_draw
-use_space:
-    mov si, offset font_SPACE
-    jmp svc01_draw
-
-svc01_draw:
-    mov cx, bx
-    mov dx, di
-    call draw_font_8x8
-    add bx, 10
-    inc word ptr [si] ; (no tiene efecto aquí, solo para balance) ; <-- lo removí abajo
-
-svc01_skip_char:
-    inc word ptr [si] ; dummy (no se usa, pero mantenemos flujo)
-    inc si            ; avanzar en la cadena ES:SI
-    jmp svc01_loop
-
-svc01_done:
-    ; terminar servicio
-    jmp svc01_exit
-
-; ------------------------------------------------
-encadenar_viejo:
-    pushf
-    push word ptr [old60_seg]
-    push word ptr [old60_ofs]
-    retf
-
-svc01_exit:
-    ; restaurar segmentos/registros en orden inverso al push
-    pop es
-    pop ds
-    pop bp
-    pop di
-    pop si
-    pop dx
-    pop cx
-    pop bx
-    pop ax
-    iret
 int60_handler endp
 
-msg db 'INT60 TSR instalado$'
+msg db 'TSR instalado!$'
 
-; FIN DEL BLOQUE RESIDENTE: etiqueta que marca el último byte que debe quedar residente
-FinResidente label byte
-
-; --------------------------
-; MAIN: instalar vector y dejar residente
-; --------------------------
-main:
-    ; asegurar CS=DS=ES para la instalación (código del TSR)
+; INSTALACIÓN
+instalar:
     mov ax, cs
     mov ds, ax
     mov es, ax
-
-    ; obtener vector viejo INT 60h
     mov ah, 35h
     mov al, 60h
     int 21h
-    mov [old60_ofs], bx
-    mov [old60_seg], es
-
-    ; instalar nuestro handler (INT 60h)
+    mov [old_int60_offset], bx
+    mov [old_int60_segment], es
     mov dx, offset int60_handler
     mov ah, 25h
     mov al, 60h
     int 21h
-
-    ; imprimir mensaje
-    mov ah, 09h
+    mov ah, 9
     mov dx, offset msg
     int 21h
-
-    ; calcular parágrafos a quedar residente (desde org 100h hasta FinResidente)
-    mov dx, offset FinResidente
+    mov dx, offset instalar
     add dx, 15
     shr dx, 4
-
-    ; dejar residente (AH=31h)
     mov ax, 3100h
     int 21h
 
