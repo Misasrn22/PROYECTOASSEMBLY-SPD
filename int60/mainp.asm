608e88b48222ee1267c7570dd9bd98eebf3b17ff
Leandro Rios <100501693+lEANDROmati@users.noreply.github.com>
2025-11-19 16:12:28 -0300

    FUNCIONA

--- int60/mainp.asm
+++ int60/mainp.asm
@@ -3,34 +3,46 @@
 .stack 100h
 
 .data
-CADENA db "AAAAA0 A0$"    
+opcion1 db "1. JUGAR$"
+opcion2 db "2. PUNTUACIONES$" 
+opcion3 db "3. SALIR$"
+cursor  db ".$"
 
 .code
 main proc
     mov ax, @data
     mov ds, ax
-    mov es, ax        ; IMPORTANTE para el TSR
-
-    ; modo gráfico 13h
-    mov ax, 0013h
+    mov es, ax
+    
+    mov ax, 13h
     int 10h
-
-    ; llamar INT 60h
+    
     mov ah, 01h
-    mov bl, 12        ; color
-    mov cx, 0        ; X
-    mov dx, 60        ; Y
-    mov si, offset CADENA
+    mov bl, 15       ; Cyan
+    mov cx, 100
+    mov dx, 60
+    mov si, offset opcion1
     int 60h
 
-    ; esperar tecla
-    mov ah, 00h
+    mov dx, 80
+    mov si, offset opcion2  
+    int 60h
+
+    mov dx, 100
+    mov si, offset opcion3
+    int 60h
+
+; Cursor de selección
+    mov bl, 1        ; Amarillo
+    mov cx, 80
+    mov dx, 60
+    mov si, offset cursor
+    int 60h
+    
+    mov ah, 0
     int 16h
-
-    ; volver a modo texto
-    mov ax, 0003h
+    mov ax, 3
     int 10h
-
     mov ax, 4C00h
     int 21h
 main endp
